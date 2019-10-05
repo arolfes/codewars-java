6000dd3b50894802ed3390e45414f0ef07b94c3e
Alex <arolfes@users.noreply.github.com>
2019-10-05 22:30:36 +0200

    Best Practice | Multiples of 3 or 5
    https://www.codewars.com/kata/514b92a657cdc65150000006/solutions/java/all/best_practices

--- src/main/java/com/github/arolfes/codewars/MultiplesOfThreeOrFive.java
+++ src/main/java/com/github/arolfes/codewars/MultiplesOfThreeOrFive.java
@@ -4,20 +4,12 @@ public class MultiplesOfThreeOrFive {
 
   public int solution(int number) {
     int sum = 0;
-    for (int i = 1; i < number; i++) {
-      if (isModuloThree(i) || isModuloFive(i)) {
+
+    for (int i = 0; i < number; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
         sum += i;
       }
     }
-    
     return sum;
   }
-  
-  private boolean isModuloThree(int num) {
-    return num % 3 == 0;
-  }
-  private boolean isModuloFive(int num) {
-    return num % 5 == 0;
-  }
-  
 }
