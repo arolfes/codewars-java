200db8c6cabefe29a4d63b253b08fdf6d2d74bfc
Alex <arolfes@users.noreply.github.com>
2019-10-07 08:47:24 +0200

    Best Practice | Flatten and sort an array
    https://www.codewars.com/kata/57ee99a16c8df7b02d00045f/solutions/java/all/best_practices

--- src/main/java/com/github/arolfes/codewars/FlattenAndSortAnArray.java
+++ src/main/java/com/github/arolfes/codewars/FlattenAndSortAnArray.java
@@ -1,7 +1,6 @@
 package com.github.arolfes.codewars;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 /**
  * https://www.codewars.com/kata/57ee99a16c8df7b02d00045f
@@ -9,12 +8,7 @@ import java.util.stream.IntStream;
 public class FlattenAndSortAnArray {
 
   public static int[] flattenAndSort(int[][] array) {
-    return flattenToInt(array).sorted().toArray();
+    return Arrays.stream(array).flatMapToInt(Arrays::stream).sorted().toArray();
   }
 
-  private static IntStream flattenToInt(Object o) {
-    return o instanceof int[] ? Arrays.stream((int[]) o)
-        : o instanceof Object[] ? Arrays.stream((Object[]) o).flatMapToInt(x -> flattenToInt(x)) 
-            : IntStream.of(((Number) o).intValue());
-  }
 }
